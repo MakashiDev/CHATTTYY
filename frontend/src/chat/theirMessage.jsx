51311d029c27faadb57b036c53900a74033daa48
Boston <yigablademaster@icloud.com>
2023-12-12 15:49:25 -0700

    change message padding

--- frontend/src/chat/theirMessage.jsx
+++ frontend/src/chat/theirMessage.jsx
@@ -7,7 +7,7 @@ function TheirMessage(props) {
         <span className="max-w-fit text-white">
           {props.message.username}
         </span>
-        <div className="ml-5 w-fit max-w-fit rounded-lg bg-gray-600 px-5 text-white">
+        <div className="ml-5 w-fit max-w-fit rounded-lg bg-gray-600 px-2 text-white">
           <p>{props.message.message}</p>
         </div>
       </div>
